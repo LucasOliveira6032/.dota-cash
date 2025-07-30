527edc4789a288ccea88327c782bd4ad1901cf15
Lucas de Oliveria <157735351+Ducaix@users.noreply.github.com>
2025-07-30 15:16:27 -0300

    Tentativa 4 de correção de acesso entre backend e banco

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,19 @@
-const app = require('./App'); 
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 3001;
+const cors = require('cors');
 
+
+app.use(cors({
+  origin: 'https://dota-cash-3p88.vercel.app', // ou '*', para testes
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
+
+app.use(express.json());
+
+const loginRoutes = require('./routes/login');
+app.use('/', loginRoutes);
 
 // Outras rotas, como /produtos, etc.
 app.listen(port, () => {
