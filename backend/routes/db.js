8eb25c5ae75e023479c34b6ae03b5756b51ae1c7
Lucas de Oliveria <157735351+Ducaix@users.noreply.github.com>
2025-07-30 14:13:28 -0300

    Tentativa 1 de correção de acesso entre backend e banco

--- backend/routes/db.js
+++ backend/routes/db.js
@@ -1,11 +1,14 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT)
+  host: process.env.MSQLHOST,
+  user: process.env.MSQLUSER,
+  password: process.env.MSQLPASSWORD,
+  database: process.env.MSQLNAME,
+  port: Number(process.env.MSQLPORT),
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = pool;
